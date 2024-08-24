459219d741c04e700efd8c784de180a37c351431
Rahul-Ram-03 <rahul.ram.2k3@gmail.com>
2024-08-24 23:03:24 +0530

    Bootstrap migrate; improved search res display; upload button; search res hyperlink

--- meta-analysis-ui/src/app/home-page/search-results/search-results.component.ts
+++ meta-analysis-ui/src/app/home-page/search-results/search-results.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { SearchResult } from '../../shared/models/search-result';
 
 @Component({
@@ -6,13 +6,7 @@ import { SearchResult } from '../../shared/models/search-result';
   templateUrl: './search-results.component.html',
   styleUrl: './search-results.component.css'
 })
-export class SearchResultsComponent implements OnChanges{
+export class SearchResultsComponent {
 
     @Input() searchResults!: SearchResult[];
-
-    ngOnChanges(changes: SimpleChanges): void {
-        if(changes) {
-            console.log({ changes });
-        }
-    }
 }
