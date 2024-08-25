418f6ed9194ad80314bfb3820f838a5f976559af
Rahul-Ram-03 <rahul.ram.2k3@gmail.com>
2024-08-25 11:01:44 +0530

    routing changes for MVP and added title

--- meta-analysis-ui/src/app/app-routing.module.ts
+++ meta-analysis-ui/src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-    { path: '', loadChildren: () => import('./login-page/login-page.module').then(m => m.LoginPageModule) },
-    { path: 'home', loadChildren: () => import('./home-page/home-page.module').then(m => m.HomePageModule) }
+    { path: 'login', loadChildren: () => import('./login-page/login-page.module').then(m => m.LoginPageModule) },
+    { path: '', loadChildren: () => import('./home-page/home-page.module').then(m => m.HomePageModule) }
 ];
 
 @NgModule({
