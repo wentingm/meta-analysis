5caaf7b78267148b989c0d7f4c1e00f942aa2c0c
Liu Liu <liuliu.nju@gmail.com>
2024-08-25 11:58:18 -0400

    pdf link

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for, session, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 import json
 from api_client import fetch_data
