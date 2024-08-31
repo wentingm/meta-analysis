12f9a56e09b1105229331b49bb08d85039a4a090
Rahul-Ram-03 <rahul.ram.2k3@gmail.com>
2024-08-31 21:32:55 +0530

    changed api endpoint url

--- meta-analysis-ui/src/app/shared/services/search.service.ts
+++ meta-analysis-ui/src/app/shared/services/search.service.ts
@@ -12,7 +12,7 @@ export class SearchService {
     private httpHeader!: HttpHeaders;
 
     constructor(private httpClient: HttpClient, private router: Router) {
-        this.backendURL = 'http://20.9.141.201:5000/process_json';
+        this.backendURL = 'https://backendapi-gxa9frakd5g4ejew.eastus-01.azurewebsites.net:5000/process_json';
     }
 
     sendPicoSearchParams(searchParams: PicoSearchQuery) {
