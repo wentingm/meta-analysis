946683c81c045d6660c1af539e38131385535198
Rahul-Ram-03 <rahul.ram.2k3@gmail.com>
2024-08-31 16:56:23 +0530

    Fix

--- meta-analysis-ui/src/app/shared/services/search.service.ts
+++ meta-analysis-ui/src/app/shared/services/search.service.ts
@@ -12,7 +12,7 @@ export class SearchService {
     private httpHeader!: HttpHeaders;
 
     constructor(private httpClient: HttpClient, private router: Router) {
-        this.backendURL = 'https://20.9.141.201:5000/process_json';
+        this.backendURL = 'http://20.9.141.201:5000/process_json';
     }
 
     sendPicoSearchParams(searchParams: PicoSearchQuery) {
