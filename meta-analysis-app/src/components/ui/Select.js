ebd8261f10bf5e772ad8829340d4261b5581d428
Swathi Nayudu <swatin74@gmail.com>
2024-12-30 13:36:52 -0500

    Updated new user pages.

--- meta-analysis-app/src/components/ui/Select.js
+++ meta-analysis-app/src/components/ui/Select.js
@@ -1,7 +1,6 @@
-// src/components/ui/select.js
 import React from 'react';
 
-const Select = ({ options, value, onChange, style, ...props }) => (
+const Select = ({ options = [], value, onChange, style, ...props }) => (
   <select
     value={value}
     onChange={onChange}
