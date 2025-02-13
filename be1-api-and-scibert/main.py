7ad7b37fb029ffe43c6fd6d3a65596d36dc6d247
kylehuang041 <kylehuang041@gmail.com>
2025-02-12 18:11:19 -0800

    changed folder name

--- be1-api-and-scibert/main.py
+++ be1-api-and-scibert/main.py
@@ -9,5 +9,9 @@ app.include_router(scibert.router, prefix="/api")
 app.include_router(semanticscholar.router, prefix="/api")
 
 
+app.get("/")
+def testing():
+    return { "response": 200 }
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
