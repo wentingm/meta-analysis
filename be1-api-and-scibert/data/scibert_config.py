18efbc7d063bf15a931d6e4810aefa38c0feec16
kylehuang041 <kylehuang041@gmail.com>
2025-02-21 13:34:53 -0800

    changed data '

--- be1-api-and-scibert/data/scibert_config.py
+++ be1-api-and-scibert/data/scibert_config.py
@@ -1,5 +1,6 @@
 config = {
   "MAX_LENGTH": 512,
-  "huggingface_pretrained_nlp_model": "allenai/scibert_scivocab_uncased",
+  "huggingface_pretrained_nlp_model": "all-MiniLM-L6-v2",
+  # "huggingface_pretrained_nlp_model": "allenai/scibert_scivocab_uncased",
   "similarity_score_threshold": 0.65
 }
